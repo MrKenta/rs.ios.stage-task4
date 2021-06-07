afacb7a07c222736f3ee59d13689d5d4584e561d
Raman Krutsiou <>
2021-06-07 22:29:08 +0300

    Add first ands second task

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,59 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        if (self < 1 || self > 3999) {
+            return nil
+        }
+        
+        var resutlString = ""
+        var number = self
+        while number > 0 {
+            switch number {
+            case 1000...3999:
+                resutlString.append("M")
+                number -= 1000
+            case 900...999:
+                resutlString.append("CM")
+                number -= 900
+            case 500...899:
+                resutlString.append("D")
+                number -= 500
+            case 400...499:
+                resutlString.append("CD")
+                number -= 400
+            case 400...499:
+                resutlString.append("CD")
+                number -= 400
+            case 100...399:
+                resutlString.append("C")
+                number -= 100
+            case 90...99:
+                resutlString.append("XC")
+                number -= 90
+            case 50...89:
+                resutlString.append("L")
+                number -= 50
+            case 40...49:
+                resutlString.append("XL")
+                number -= 40
+            case 10...39:
+                resutlString.append("X")
+                number -= 10
+            case 9:
+                resutlString.append("IX")
+                number -= 9
+            case 5...8:
+                resutlString.append("V")
+                number -= 5
+            case 4:
+                resutlString.append("IV")
+                number -= 4
+            case 1...3:
+                resutlString.append("I")
+                number -= 1
+            default:
+                return nil
+            }}
+        return resutlString
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -2,25 +2,60 @@ import Foundation
 
 final class CallStation { }
 
+var  usersBook = [User]()
+var  callList = [Call]()
+var  currentCallList:[UUID: Call] = [:]
+
 extension CallStation: Station {
     func users() -> [User] {
-        []
+        usersBook
     }
     
     func add(user: User) {
-
+        switch usersBook.contains(user) {
+        case false:
+            usersBook.append(user)
+        default:
+            break
+        }
     }
     
     func remove(user: User) {
-
+        for i in 0...usersBook.count - 1 {
+            if user == usersBook[i] {
+                usersBook.remove(at:i)
+            }
+        }
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        
+        switch action {
+        case .start(from: let userOne, to: let userTwo):
+            if usersBook.contains(userOne) && usersBook.contains(userTwo) {
+                let call = Call(id:UUID(), incomingUser:userOne, outgoingUser:userTwo, status:.calling)
+                callList.append(call)
+                return call.id
+            }
+                if !usersBook.contains(userOne) || !usersBook.contains(userTwo) {
+                    let callOne = Call(id:UUID(), incomingUser: userOne, outgoingUser: userTwo, status:.ended(reason:.error))
+                    callList.append(callOne)
+                    return callOne.id
+                }
+            if self.currentCall(user:userTwo) != nil {
+                let callTwo = Call(id:UUID(), incomingUser:userOne, outgoingUser:userTwo, status:.ended(reason:.userBusy))
+                callList.append(callTwo)
+            }
+        case .answer(from: let userOne):
+            let status = currentCall(user:userOne)
+        default:
+            break
+        }
+        return nil
     }
     
     func calls() -> [Call] {
-        []
+        callList
     }
     
     func calls(user: User) -> [Call] {
@@ -32,6 +67,6 @@ extension CallStation: Station {
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        currentCallList[user.id]
     }
 }
